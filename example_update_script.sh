434b3184b7d13da0714bfc5b6aefb4e06c64d5f1
Steven Saus <steven@stevesaus.com>
2026-07-20 14:46:55 -0400

    Added an example of how to update dynamically

---
+++ example_update_script.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This is an example of how you can do dynamic updates.  This pulls a 
+# data value from a cached file, parses it, 
+# prepends an icon, and then updates the second line ONLY of 
+# your current rich presence (if RPC is being handled by geneassay)
+
+
+PressureScore=$(head -n 1 /home/steven/.cache/tanuki_weather/weather_cache/Phonepressure_pressure_score.txt)
+PressureLevel=$(awk -v score="$PressureScore" 'BEGIN {
+        if      (score < 0.50) level = 1
+        else if (score < 1.00) level = 2
+        else if (score < 1.50) level = 3
+        else if (score < 2.00) level = 4
+        else if (score < 2.50) level = 5
+        else if (score < 3.00) level = 6
+        else                   level = 7
+
+        print level
+    }'
+)
+
+case "$PressureLevel" in 
+	1) icon="🟪";;
+	2) icon="🟦";;
+	3) icon="🟩";;
+	4) icon="🟨";;
+	5) icon="🟧";;
+	6) icon="🟥";;
+	7) icon="◻️";;
+	*) icon="🟫";;
+esac
+
+out_text=$(printf "pressure: %s %s" "$icon" "$PressureScore")
+
+
+
+./geneassay/dynamic_update.sh --line2="${out_text}"
